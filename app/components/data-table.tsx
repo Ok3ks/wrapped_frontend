a0bb36ac096c8d57da5576d61f5222ca2c2ed488
ok3ks <freelanceokeks@gmail.com>
2025-10-14 14:50:31 +0100

    Created Data Table

---
+++ app/components/data-table.tsx
@@ -0,0 +1,191 @@
+"use client"
+
+import {useState} from "react"
+import {
+    type ColumnDef, 
+    type ColumnFiltersState,
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    type SortingState,
+    useReactTable,
+    type VisibilityState,
+} from "@tanstack/react-table"
+
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
+import { Button } from "../components/ui/button"
+import { Checkbox } from "../components/ui/checkbox"
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from  "../components/ui/dropdown-menu"
+import { Input } from "../components/ui/input"
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow
+} from "../components/ui/table"
+
+export type Players = {
+    id: string,
+    name: string,
+    team: string,
+    points: number,
+}
+
+export const columns: ColumnDef<Players>[] = [
+
+    {
+        accessorKey: "name",
+        header: "name",
+    },
+    {
+        accessorKey: "points",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "desc")}>
+                points
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+                )
+            },
+    },
+    {
+        accessorKey: "team",
+        header: "team",
+    },
+]
+
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[]
+    data: TData[]
+}
+
+export function DataTable<TData, TValue>({
+    columns,
+    data,
+}: DataTableProps<TData, TValue>) {
+    const [sorting, setSorting] = useState<SortingState>([]);
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+    const [pagination, setPagination] = useState({
+        pageIndex: 0, //initial page index
+        pageSize: 21, //default page size
+      });
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        getFilteredRowModel: getFilteredRowModel(),
+        onPaginationChange: setPagination,
+        rowCount: 20,
+        state: {
+            sorting,
+            columnFilters
+        }
+    })
+
+    return (
+        <div>
+            <div className="flex items-center py-4">
+                <Input
+                    placeholder="Search Player Name"
+                    value={
+                        (table.getColumn("name")?.getFilterValue() as string ?? "")
+                    }
+                    onChange={(event) => {
+                        table.getColumn("name")?.setFilterValue(event.target.value) 
+                    }
+                    }
+                    className="max-w-sm"
+                    />
+            </div>
+        <div className="gameweek-table">
+            <Table>
+                <TableHeader>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => {
+                                return (
+                                    <TableHead key={header.id}>
+                                        {header.isPlaceholder
+                                        ? null 
+                                        : flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )
+                                    
+                                    }
+                                    </TableHead>
+                                )
+                            })}
+                        </TableRow>
+                    ))}
+                </TableHeader>
+
+                <TableBody>
+                    {table.getRowModel().rows?.length 
+                    ? (
+                        table.getRowModel().rows.map((row) => (
+                            <TableRow
+                            key={row.id}
+                            data-state={row.getIsSelected() && "selected"}
+                            >
+
+                                {row.getVisibleCells().map((cell) => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </TableCell>
+                                ))
+                                }
+                            </TableRow>
+                        ))
+                    )
+                    : (
+                        <TableRow>
+                            <TableCell colSpan={columns.length} className="h-24 text-center">
+                                No results.
+                            </TableCell>
+                        </TableRow>
+                    )
+                
+                }
+                </TableBody>
+            </Table>
+        </div>
+        <div className="flex items-center justify-end space-x-2 py-4">
+            <Button
+                variant="outline"
+                size="sm"
+                onClick={() => table.previousPage()}
+                disabled={!table.getCanPreviousPage()}
+            >
+                Previous
+            </Button>
+            <Button
+                variant="outline"
+                size="sm"
+                onClick={() => table.nextPage}
+                disabled={!table.getCanNextPage()}
+            >
+                Next
+            </Button>
+        </div>
+        </div>
+    )
+}
--- app/components/gameweek-tile.tsx
+++ app/components/gameweek-tile.tsx
@@ -1,13 +1,47 @@
 
+import { columns, type Players, DataTable } from "./data-table";
+
 interface gameweekTileProps {
     gameweek: number,
 }
 
-export default function gameweekTile({gameweek}: gameweekTileProps) {
-
+async function getGameweekData() : Promise<Players[]> {
+    return [
+        {
+            id: "1",
+            name: "Salah",
+            team: "Liverpool",
+            points: 11,
+        },
+        {
+            id: "2",
+            name: "Sarr",
+            team: "Crystal Palace",
+            points: 80,
+        },
+        {
+            id: "3",
+            name: "Mateta",
+            team: "Crystal Palace",
+            points: 70,
+        },
+        {
+            id: "4",
+            name: "Saka",
+            team: "Crystal Palace",
+            points: 70,
+        }
+    ]
+}
+export default async function gameweekTile({gameweek}: gameweekTileProps) {
+    const data = await getGameweekData();
     return (
             <div className="gameweek-tile">
                 <h2> Gameweek {gameweek}</h2>
+                <div>
+                    <DataTable columns={columns} data={data} />
+                </div>
+
             </div>
         
     );
